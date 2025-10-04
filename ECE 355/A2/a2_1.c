c5aaf6b24e893d16a2dcbf9db1a3edc5decb1b81
amirabdullahi1 <128766411+amirabdullahi1@users.noreply.github.com>
2025-10-04 13:55:21 -0700

    Update a2_1.c

--- ECE 355/A2/a2_1.c
+++ ECE 355/A2/a2_1.c
@@ -1,13 +1,16 @@
 #define PAIN (volatile unsigned char *) 0xFFFFFFF0
 #define PAOUT (volatile unsigned char *) 0xFFFFFFF1
 #define PADIR (volatile unsigned char *) 0xFFFFFFF2
-#define PBIN (volatile unsigned char *) 0xFFFFFFF3
 #define PBOUT (volatile unsigned char *) 0xFFFFFFF4
 #define PBDIR (volatile unsigned char *) 0xFFFFFFF5 
+#define PCONT (volatile unsigned char *) 0xFFFFFFF7 
 #define CNTM (volatile unsigned int *) 0xFFFFFFD0 
 #define CTCON (volatile unsigned char *) 0xFFFFFFD8 
 #define CTSTAT (volatile unsigned char *) 0xFFFFFFD9 
 #define IVECT (volatile unsigned int *) (0x20)
+#define LED1MASK (0x04)
+#define LED2MASK (0x01)
+#define SWMASK (0x80)
 
 interrupt void intserv(); 
 
@@ -16,39 +19,46 @@ unsigned char digit_2 = 0;	            /* 2nd Digit to be displayed */
 unsigned int alt_led = 0;               /* 0 for Led1 and 1 for Led2 */
 
 int main() {
-    *PADIR = 0x78;                      /* Set Port A direction */
-    *PBDIR = 0xF5;                      /* Set Port B direction */
-    *CTCON = 0x02;                      /* Stop Timer */
-    *CTSTAT = 0x0;                      /* Clear “reached 0” flag */
-    *CNTM = 100000000;                  /* Initialize Timer */
-    *IVECT = (unsigned int *) &intserv; /* Set interrupt vector */
-    asm(“MoveControl PSR,#0x40”);       /* CPU IRQ enabled */
-    *CTCON = 0x01;                      /* Start counting w/o timer interrupt enable */
+  *PADIR = 0x78;                        /* Set Port A direction */
+  *PBDIR = 0xF5;                        /* Set Port B direction */
+  *CTCON = 0x02;                        /* Stop Timer */
+  *CTSTAT = 0x0;                        /* Clear “reached 0” flag */
+  *CNTM = 100000000;                    /* Initialize Timer */
+  *IVECT = (unsigned int *) &intserv;   /* Set interrupt vector */
+  asm("MoveControl PSR,#0x40");         /* CPU IRQ enabled */
+  *PCONT = 0x10;                        /* Enable port A interrupts */
+  *CTCON = 0x01;                        /* Start counting w/o timer interrupt enable */
+  *PAOUT &= SWMASK;                     /* Init Port A w/ &ing SWMASK so Dig1 = 0 */
+  *PBOUT = LED2MASK;                    /* Init Port B w/ LED2MASK so Led1 on, Led2 off and Dig2 = 0 */
     
-    while (1) {
-        while (*CTSTAT != 0x0);          /* Wait until 0 is reached */
+  while (1) {
+    while ((*CTSTAT & 0x1) == 0x0);     /* Wait until 0 is reached */
+    *CTSTAT = 0x0;                      /* Clear “reached 0” flag */
 
-        if ( ) {
-
-        } else {
-          digit_2 = (digit_2 + 1)%10;         /* Increment digit_2 */
-          *PBOUT = (digit_2 << 3);            /* Update Port B */
-        }
-        while ((*PBIN & 0x1) != 0);     /* Wait until E is pressed */
-        while ((*PBIN & 0x1) == 0);     /* Wait until E is released */
-        increment = 1;
-
-        while ((*PBIN & 0x2) != 0);     /* Wait until D is pressed */
-        while ((*PBIN & 0x2) == 0);     /* Wait until D is released */
-        increment = 0;
+    if (alt_led == 0) {
+      /* Increment digit_1 */
+      digit_1 = (digit_1 + 1)%10;    
+      /* Update Port A w/o changing SW */                
+      *PAOUT = (unsigned char)((digit_1 << 3) | (*PAOUT & SWMASK)); 
+    } else {
+      /* Increment digit_2 */
+      digit_2 = (digit_2 + 1)%10;
+      /* Update Port B so Led1 is off and Led2 is on */                               
+      *PBOUT = (unsigned char)((digit_2 << 4) | LED1MASK);          
     }
+  }
 }
 
 interrupt void intserv() {
-    *CTSTAT = 0x0;                      /* Clear “reached 0” flag */
-
-    if (increment == 1) {
-        digit = (digit + 1)%10;         /* Increment digit */
-        *PBOUT = ((digit << 4) | 0x0);  /* Update Port B */
-    }
+  if ((*PAIN & SWMASK) != 0) {        /* Only alt when SW releasd */
+      if (alt_led == 0) {
+        *PBOUT |= LED1MASK;         /* Turn off Led1 */
+        *PBOUT &= ~LED2MASK;        /* Turn on Led2 */
+        alt_led = 1;
+      } else {
+        *PBOUT &= ~LED1MASK;        /* Turn on Led1 */
+        *PBOUT |= LED2MASK;         /* Turn off Led2 */
+        alt_led = 0;
+      }
+  }
 }
