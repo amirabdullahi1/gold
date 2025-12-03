8e55cf7af36152024d00a1583fa96c5e5c3eaa01
amirabdullahi1 <128766411+amirabdullahi1@users.noreply.github.com>
2025-12-03 14:19:32 -0800

    Add parallel dot product function with barrier sync
    Implement parallel dot product computation using threads.

--- ECE 355/A6/a6_4.c
+++ ECE 355/A6/a6_4.c
@@ -1,1 +1,34 @@
-
+#include <stdio.h>                                        /* Routines for input/output. */
+#include "threads.h"                                      /* Routines for thread creation/synchronization. */
+#define N 100                                             /* Number of elements in each vector. */
+#define P 4                                               /* Number of processors for parallel execution. */
+double a[N], b[N];                                        /* Vectors for computing the dot product. */
+double partial_sums[P];                                   /* Array of results computed by threads. */
+Barrier bar;                                              /* Shared variable to support barrier synchronization. */
+void ParallelFunction (void)
+{
+  int my_id, i, start, end;
+  double s;
+  my_id = get_my_thread_id ();                            /* Get unique identifier for this thread. */
+  start = (N/P) * my_id;                                  /* Determine start/end using thread identifier. */
+  end = (N/P) * (my_id + 1) – 1;                          /* N is assumed to be evenly divisible by P .* /
+  s = 0.0;
+  for (i = start; i <= end; i++)
+    s = s + a[i] * b[i];
+  partial_sums[my_id] = s;                                 /* Save result in array. */
+  barrier (&bar, P);                                       /* Synchronize with other threads. */
+}
+void main (void)
+{
+  int i;
+  double dot_product;
+  < Initialize vectors a[], b[] – details omitted.>
+  init_barrier (&bar);
+  for (i = 1; i < P; i++)                                   /* Create P – 1 additional threads. */
+    create_thread (ParallelFunction);
+  ParallelFunction();                                       /* Main thread also joins parallel execution. */
+  dot_product = 0.0;                                        /* After barrier synchronization, compute final result. */
+  for (i = 0; i < P; i++)
+    dot_product = dot_product + partial_sums[i];
+  printf ("The dot product is %g\ n", dot_product);
+}
