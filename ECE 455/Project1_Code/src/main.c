1cd980018d2cc5186633b006129e170dfb26e80f
amirabdullahi1 <128766411+amirabdullahi1@users.noreply.github.com>
2026-02-06 12:06:25 -0800

    Change printf format specifier for duration values

--- ECE 455/Project1_Code/src/main.c
+++ ECE 455/Project1_Code/src/main.c
@@ -239,9 +239,9 @@ static void light_state_task ( void *pvParameters ) {
                 {
                     r_dur = 9999.0 * (2 - ADC_val/ADC_MAX);
                     g_dur = 9999.0 * (1 + ADC_val/ADC_MAX);
-                	printf("r_dur %u.\n", r_dur);
-                	printf("y_dur %u.\n", y_dur);
-                	printf("g_dur %u.\n", g_dur);
+                	printf("r_dur %.5f.\n", r_dur);
+                	printf("y_dur %.5f.\n", y_dur);
+                	printf("g_dur %.5f.\n", g_dur);
                 }
             }
 
