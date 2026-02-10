cb397ea9e47d3254b07b400e6480b3d5a8239004
amirabdullahi1 <128766411+amirabdullahi1@users.noreply.github.com>
2026-02-09 16:09:23 -0800

    Remove timer configuration macros and add heap size

--- ECE 455/Project1_Code/src/main.c
+++ ECE 455/Project1_Code/src/main.c
@@ -31,10 +31,7 @@
 
 #define ADC_MAX 4095
 
-#define configUSE_TIMERS 1
-#define configTIMER_TASK_PRIORITY        2
-#define configTIMER_QUEUE_LENGTH         5
-#define configTIMER_TASK_STACK_DEPTH     256
+#define configTOTAL_HEAP_SIZE (20 * 1024)
 
 // Added for traffic managment.
 #define NUM_TRAFFIC_LEDS 19
