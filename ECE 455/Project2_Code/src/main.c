9a44cf00787611a3d06eea044c1145305248c00a
amirabdullahi1 <128766411+amirabdullahi1@users.noreply.github.com>
2026-03-09 16:50:46 -0700

    Update main.c

--- ECE 455/Project2_Code/src/main.c
+++ ECE 455/Project2_Code/src/main.c
@@ -1,6 +1,7 @@
 /* Standard includes. */
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "stm32f4_discovery.h"
 /* Kernel includes. */
 #include "stm32f4xx.h"
@@ -9,7 +10,15 @@
 #include "../FreeRTOS_Source/include/semphr.h"
 #include "../FreeRTOS_Source/include/task.h"
 #include "../FreeRTOS_Source/include/timers.h"
+/*-----------------------------------------------------------*/
 
+
+/*---- Pragmas ----------------------------------------------*/
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-function"
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+/*-----------------------------------------------------------*/
 
 
 /*-----------------------------------------------------------*/
@@ -42,9 +51,9 @@ typedef struct dd_task_list {
 	struct dd_task_list *next_task;
 } dd_task_list;
 
-dd_task_list active_task_list;
-dd_task_list completed_task_list;
-dd_task_list overdue_task_list;
+dd_task_list *active_task_list = NULL;
+dd_task_list *completed_task_list = NULL;
+dd_task_list *overdue_task_list = NULL;
 /*-----------------------------------------------------------*/
 
 /*-----------------------------------------------------------*/
@@ -60,7 +69,7 @@ void dd_task_list_add(dd_task_list **task_list, dd_task this_task) {
 	dd_task_list *task_list_curr = *task_list;
 
 	dd_task curr_task;
-	while(task_list_curr != NULL) 
+	while(task_list_curr != NULL)
 	{
 		curr_task = task_list_curr->task;
 		if(this_task.absolute_deadline < curr_task.absolute_deadline)
@@ -72,66 +81,90 @@ void dd_task_list_add(dd_task_list **task_list, dd_task this_task) {
 
 	task_list_this->next_task = task_list_curr;
 
-	if(task_list_prev == NULL) 
+	if(task_list_prev == NULL)
 		*task_list = task_list_this;
-	else 
+	else
 		task_list_prev->next_task = task_list_this;
 }
 
 void dd_task_list_rmv(dd_task_list **task_list, dd_task this_task) {
 	dd_task_list *task_list_prev = NULL;
 	dd_task_list *task_list_curr = *task_list;
-	
+
 	dd_task curr_task;
-	while(task_list_curr != NULL) 
+	while(task_list_curr != NULL)
 	{
 		curr_task = task_list_curr->task;
 		if(this_task.t_handle == curr_task.t_handle)
 			break;
-		
+
 		task_list_prev = task_list_curr;
 		task_list_curr = task_list_curr->next_task;
 	}
 
 	if(task_list_curr == NULL)
 		return;
-	if(task_list_prev == NULL) 
+	if(task_list_prev == NULL)
 		*task_list = task_list_curr->next_task;
-	else 
+	else
 		task_list_prev->next_task = task_list_curr->next_task;
 
 	free(task_list_curr);
 }
 /*-----------------------------------------------------------*/
-void create_dd_task(TaskHandle_t t_handle, task_type type, uint32_t task_id, uint32_t absolute_deadline){
-	dd_message msg;
-    msg.msg_type = DD_MSG_CREATE;
+/* Create and delete task functions */
 
-    msg.task.t_handle = t_handle;
-    msg.task.task_id = task_id;
-    msg.task.type = type;
-    msg.task.release_time = (uint32_t)xTaskGetTickCount();
-    msg.task.absolute_deadline = absolute_deadline;
-    msg.task.completion_time = 0;
+void create_dd_task(TaskHandle_t t_handle, task_type type, uint32_t task_id, uint32_t absolute_deadline)
+{
+	dd_task *new_task = malloc(sizeof(dd_task));
+	new_task->t_handle = t_handle;
+	new_task->type = type;
+	new_task->task_id = task_id;
+	new_task->release_time = (uint32_t)xTaskGetTickCount();
+	new_task->absolute_deadline = absolute_deadline;
+	new_task->completion_time = 0; // Setting as 0 so that it does not take on some random value until it can be assigned properly upon actual completion.
 
-    if(xQueueSend(xDDS_Queue, &msg, 1000) != pdPASS)
-    	printf("create_dd_task: failed when sending to DDS queue\n");
+	dd_task_list_add(&active_task_list, *new_task); // Calls the function to add to the task list.
+	free(new_task);
 }
 
-void delete_dd_Task(uint32_t task_id){
-	dd_message msg;
-    msg.msg_type = DD_MSG_DELETE;
-    msg.task.task_id = task_id;
-    msg.task.completion_time = (uint32_t)xTaskGetTickCount();
+// Have this here for reference for now (DELETE WHEN COMPLETED!!!!)
+//typedef struct dd_task_list {
+//	dd_task task;
+//	struct dd_task_list *next_task;
+//} dd_task_list;
 
-    if(xQueueSend(xDDS_Queue, &msg, 1000) != pdPASS)
-    	printf("delete_dd_Task: failed when sending to DDS queue\n");
+void delete_dd_task(uint32_t task_id)
+{
+//	if(){
+//		printf("There is nothing to remove the task list is empty.\n");
+//		return;
+//	}
+//
+//	// Need to understand where the head for the specific task list is to delete properly...
+//	dd_task_list head = active_task_list;
+//	if(head.task.task_id == task_id){
+//		printf("Task %d is being removed", task_id);
+//	}else{
+////		prev = head;
+//		head = head->next_task;
+//
+//	}
+
 }
+
+/*-----------------------------------------------------------*/
+
+
 
 
 
 /*-----------------------------------------------------------*/
-static void Manager_Task( void *pvParameters );
+static void DD_Task_Generator( void *pvParameters );
+static void DD_Task1( void *pvParameters );
+static void DD_Task2( void *pvParameters );
+static void DD_Task3( void *pvParameters );
+
 xQueueHandle xQueue_handle = 0;
 /*-----------------------------------------------------------*/
 
@@ -151,18 +184,59 @@ int main(void)
 	/* Add to the registry, for the benefit of kernel aware debugging. */
 	vQueueAddToRegistry( xQueue_handle, "MainQueue" );
 
-	xTaskCreate( Manager_Task, "Manager", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+	xTaskCreate( DD_Task_Generator, "DD Task Gen", 256, NULL, 2, NULL);
 
 	/* Start the tasks and timer running. */
 	vTaskStartScheduler();
 
 	return 0;
 }
+/*-----------------------------------------------------------*/
 
+
+static void DD_Task_Generator(void *pvParameters)
+{
+	const uint32_t *params = (uint32_t *)pvParameters;
+	uint32_t period_ticks;
+	if(params != NULL){
+		period_ticks = *params;
+	}else{
+		period_ticks = pdMS_TO_TICKS(500);
+	}
+	static uint32_t task_id_counter = 0;
+	TaskHandle_t new_task_handle1 = NULL;
+	TaskHandle_t new_task_handle2 = NULL;
+	TaskHandle_t new_task_handle3 = NULL;
+
+
+// Test bench 1 LCM 1500
+//	uint32_t DD_Task1_Period = 500
+//	uint32_t DD_Task2_Period = 500
+//	uint32_t DD_Task3_Period = 750
+
+	xTaskCreate(DD_Task1, "DD_Task1", configMINIMAL_STACK_SIZE, NULL, 1, &new_task_handle1);
+	xTaskCreate(DD_Task2, "DD_Task2", configMINIMAL_STACK_SIZE, NULL, 1, &new_task_handle2);
+	xTaskCreate(DD_Task3, "DD_Task3", configMINIMAL_STACK_SIZE, NULL, 1, &new_task_handle3);
+
+	while(1){
+
+
+		uint32_t release = (uint32_t)xTaskGetTickCount();
+		uint32_t deadline = release + period_ticks;
+
+		create_dd_task(new_task_handle1, PERIODIC, task_id_counter++, deadline);
+
+
+
+
+
+		vTaskDelay(period_ticks);
+	}
+}
 
 /*-----------------------------------------------------------*/
 
-static void Manager_Task( void *pvParameters )
+static void DD_Task1( void *pvParameters )
 {
 	uint16_t tx_data = 0;
 
@@ -173,14 +247,74 @@ static void Manager_Task( void *pvParameters )
 		if(tx_data == 0) {
 			//   Do task0.
 		}
-			
+
 		/* xQueue_handle: * xQueueSend Posts an item on queue defined by xQueue_handle
 		 * rx_data: A pointer to the item that is to be placed on the queue
-		 * 1000: The maximum amount of time the task should block waiting for space to become 
+		 * 1000: The maximum amount of time the task should block waiting for space to become
 		 */
 		if( xQueueSend(xQueue_handle,&tx_data,1000))
 		{
-			printf("Manager: %u ON!\n", tx_data);
+			printf("DD_Task1 ON!\n");
+			if(tx_data == 0)
+				tx_data = 0;
+			vTaskDelay(1000);
+		}
+		else
+		{
+			printf("Manager Failed!\n");
+		}
+	}
+}
+
+static void DD_Task2( void *pvParameters )
+{
+	uint16_t tx_data = 0;
+
+
+	while(1)
+	{
+
+		if(tx_data == 0) {
+			//   Do task0.
+		}
+
+		/* xQueue_handle: * xQueueSend Posts an item on queue defined by xQueue_handle
+		 * rx_data: A pointer to the item that is to be placed on the queue
+		 * 1000: The maximum amount of time the task should block waiting for space to become
+		 */
+		if( xQueueSend(xQueue_handle,&tx_data,1000))
+		{
+			printf("DD_Task2 ON!\n");
+			if(tx_data == 0)
+				tx_data = 0;
+			vTaskDelay(1000);
+		}
+		else
+		{
+			printf("Manager Failed!\n");
+		}
+	}
+}
+
+static void DD_Task3( void *pvParameters )
+{
+	uint16_t tx_data = 0;
+
+
+	while(1)
+	{
+
+		if(tx_data == 0) {
+			//   Do task0.
+		}
+
+		/* xQueue_handle: * xQueueSend Posts an item on queue defined by xQueue_handle
+		 * rx_data: A pointer to the item that is to be placed on the queue
+		 * 1000: The maximum amount of time the task should block waiting for space to become
+		 */
+		if( xQueueSend(xQueue_handle,&tx_data,1000))
+		{
+			printf("DD_Task3 ON!\n");
 			if(tx_data == 0)
 				tx_data = 0;
 			vTaskDelay(1000);
@@ -192,33 +326,7 @@ static void Manager_Task( void *pvParameters )
 	}
 }
 /*-----------------------------------------------------------*/
-static void DD_Task_Generator(void *pvParameters){
-	const uint32_t *params = (unint32_t *)pvParameters;
-	uint32_t period_ticks;
-	if(params != NULL){
-    	period_ticks = *params;
-	} else {
-    	period_ticks = pdMS_TO_TICKS(500);
-	}
-    static uint32_t task_id_counter = 0;
-    TaskHandle_t new_task_handle = NULL;
 
-    while(1){
-    	uint32_t release = (uint32_t)xTaskGetTickCount();
-		uint32_t deadline = release + period_ticks;
-
-        xTaskCreate(User_DD_Task, "DD_Task", configMINIMAL_STACK_SIZE, NULL, 1, &new_task_handle);
-
-        if(new_task_handle != NULL){
-        	create_dd_task(new_task_handle, PERIODIC, task_id_counter++, deadline);
-        }
-
-        vTaskDelay(period_ticks);
-    }
-
-}
-
-/*-----------------------------------------------------------*/
 
 
 void vApplicationMallocFailedHook( void )
@@ -280,3 +388,6 @@ static void prvSetupHardware( void )
 	/* TODO: Setup the clocks, etc. here, if they were not configured before
 	main() was called. */
 }
+
+/*-----------------------------------------------------------*/
+#pragma GCC diagnostic pop
