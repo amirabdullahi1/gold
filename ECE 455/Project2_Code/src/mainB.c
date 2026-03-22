6c8bef490c20ec67ab8881c7e94160e85e0d23ac
Amir Abdullahi <amirabdullahi1@uvic.ca>
2026-03-21 18:21:00 -0700

    Debug1 printf.txt

--- ECE 455/Project2_Code/src/mainB.c
+++ ECE 455/Project2_Code/src/mainB.c
@@ -258,6 +258,9 @@ void update_dd_task(dd_task_list *updater_list)
 	// if (no HI task || HI task demoted) -> promote updater_list head
     if(updater_list != NULL)
 	{
+		printf("Tid %u \n", (unsigned int)(updater_list->task.task_id));
+		printf("Abs %u \n", (unsigned int)(updater_list->task.absolute_deadline));
+		printf("Tix %u \n", (unsigned int)(xTaskGetTickCount()));
     	xQueueOverwrite(xDDS_TidQueue_Handle, &(updater_list->task.task_id));
         vTaskPrioritySet(updater_list->task.t_handle, PRIORITY_HI);
 		vTaskResume(updater_list->task.t_handle);
